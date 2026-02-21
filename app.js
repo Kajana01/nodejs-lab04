fc9418b1d5d929b090d9e64f42e09400146fc4cd
kajana pushparajah <pushparajahkajana0139@gmail.com>
2026-02-21 16:18:38 +0530

    Added custom module example

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const fs = require('fs');
-
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-    if (err) throw err;
-    console.log('File saved!');
-});
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
